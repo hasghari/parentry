9bbd671e898c5462044337f9389e88f5b99b4fb4
Hamed Asghari <hasghari@pm.me>
2025-10-12 08:03:08 -0600

    chore: Update outdated gems

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,14 +1,17 @@
 class TreeNode < ActiveRecord::Base
   include Parentry
+
   parentry cache_depth: true, strategy: ENV.fetch('STRATEGY', 'ltree')
 end
 
 class OneDepthTreeNode < ActiveRecord::Base
   include Parentry
+
   parentry depth_offset: 1, strategy: ENV.fetch('STRATEGY', 'ltree')
 end
 
 class TouchTreeNode < ActiveRecord::Base
   include Parentry
+
   parentry touch: true, strategy: ENV.fetch('STRATEGY', 'ltree')
 end
